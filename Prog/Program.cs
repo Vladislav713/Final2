c8f386f14b17053163783bbbbde647b6121233d1
Vladislav <vladperov@mail.ru>
2022-11-16 21:55:16 +0500

    4

--- Prog/Program.cs
+++ Prog/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+
+void Print (string[] array)
+{
+    for (int i = 0; i<array.Length; i++)
+        Console.Write(array[i]+" ");
+}
+
+string[] array = {"1234","567","-2","computer science"};
+
+int count=0;
+for (int i=0;i<array.Length; i++)
+{
+    if (array[i].Length<=3)
+    {
+        count++;
+    }
+}
+
+string[]array2=new string[count];
+int j=0;
+for (int i=0;i<array.Length; i++)
+{
+    if (array[i].Length<=3)
+    {
+        array2[j]=array[i];
+        j++;
+    }
+}
+
+Print(array);
+Console.WriteLine();
+Print(array2);
